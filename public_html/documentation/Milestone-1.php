a09dc2c1889ec6286f36311b52164f6431feeadc
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-15 16:57:58 -0600

    Milestone 1 Initial submission.

---
+++ public_html/documentation/Milestone-1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Project</title>
+	</head>
+	<body bgcolor="#dda0dd">
+		<h1>Personal Project</h1>
+		<h2>Bake-Me- Away</h2>
+<div style="font-size: large">
+	<h2>Purpose, Audience & Goal</h2>
+	<ul>
+		<li><strong>Purpose: </strong>This Project will provide the residents of Ahmedabad city with fresh-baked artisan breads, variety of chocolates,cakes and cookies on a daily basis.
+		<li><strong>Audience:</strong> This website is designed with a BakeryLover audience in mind.</br>	Target audience to include: City travelllers,Local Residence and EventManaging Companies. </li>
+		<li><strong>Goal:</strong>The goal of this site is user to initiate contact with business in regards place order for bakery items
+			 and to showcase my work using different digital source.
+		</li>
+	</ul>
+
+</div>
+		<div style="font-size: large">
+			<h2>Persona</h2>
+			<div>
+<img width="500" src="agile-event-manager.jpg" alt="Image of manager">
+			</div>
+			<ul>
+				<p><strong>Name</strong> Radhika Patel  </p>
+				<p><strong>Age: </strong>27</p>
+				<p><strong>Profession: </strong>Event Manager</p>
+				<p><strong>	Technology:</strong> All laptop,data connection,ipads,Mac book and Mobiles.</p>
+				<p><strong>Attitude and Behaviour:</strong>She have busy work schedule as event manager.</br>
+					She do not want to spent more than 5-8 min to place order.</br>
+					She is not willing to explain procedure of quality ,packing and delivery again and again. </p>
+				<br><strong>Frustrations and Needs:</strong> She is looking for better payment flexibility for her orders.</br>
+				She wants to set contract with one place which serve quality food and provide service on time on easy click.</br>
+				She also needs fastest delivery within 24hours of order placed.</p>
+				<p><strong>Social Media Activity :</strong>Facebook and Instagram</p>
+				<p><strong>Goals:</strong>Radhika intention is to set a contract with client for all her event with quality and flex service.
+						Can Radhika place order 5-8min max? Can Radhika get better payment plan? </p>
+
+				<p><strong>User Story:</strong> As a user, I want to search and order cakes, bakery and chocolate for my events.</p>
+</ul>
+		</div>
+
+	</body>
+</html>
