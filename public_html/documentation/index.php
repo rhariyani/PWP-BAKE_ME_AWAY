a4d6a9ce307b32f4df229649f30cf0fa6fc55cda
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-29 11:52:25 -0600

    Milestone2 submission with WireFrame.

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -9,8 +9,8 @@
 	<body style="font-size: large">
 		<h2>Welcome</h2>
 		<a href="milestone-1.php" style="color: darkolivegreen ">MileStone1</a><br>
-		<a href="milestone2.php" style="color: darkolivegreen">MileStone2</a><br>
-		<a href="wireframe.php" style="color: darkolivegreen">Wireframe for Full screen</a><br>
-		<a href="wireframemobilelook.php" style="color: darkolivegreen"> Wireframe Mobile Layout</a>
+		<a href="milestone-2.php" style="color: darkolivegreen">MileStone2</a><br>
+		<a href="wireframe.php" style="color: darkolivegreen">WireFrame Full screen</a><br>
+		<a href="wireframemobilelook.php" style="color: darkolivegreen"> WireFrame Mobile Layout</a>
 	</body>
 </html>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Project</title>
+
+	</head>
+	<body style="font-size: large">
+		<div>
+		<h2>Content Strategy</h2>
+
+			<p>My site will be one page, and will consist of the following sections:</p><br>
+
+			<p>	<strong>Welcome(Link logo of Bake-Me-Away to go home/welcome page):</strong>A short write-up about shop and owner.
+			This page will have images of some fine creations of products and a display for current offer on top of website.</p><br>
+
+			<p><strong>About Me:</strong>This section will have a short story of owner's journey from student to business man.</p><br>
+
+			<p>	<strong>Price:</strong>This section will display the price list of products sold at shop.</p><br>
+
+			<p>	<strong>Product:</strong>This section will display product images in different categories in a fancy way sold at shop.</p><br>
+
+			<p><strong>Contact:</strong>This section will contain terms and conditions for user, a contact form, email address, store location, and additional links to owner's professional
+			social media profiles:(Facebook and Instagram).<br>
+		</div>
+      <div>
+			<a href="wireframe.php" style="color: darkolivegreen">Wireframe for Full screen</a><br>
+			<a href="wireframemobilelook.php" style="color: darkolivegreen"> Wireframe Mobile Layout</a><br>
+		</div>
+
+		<div>
+		<a href="index.php">HomePage</a>
+		</div>
+	</body>
+	</html>
