655e4ebefe81585a391e06bc0cbf42c849a20983
Rishita Hariyani <rhariyani@cnm.edu>
2019-08-28 22:02:54 -0600

    Final Submission of PWP. Thank you

---
+++ documentation/wireframemobilelook.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Project</title>
+
+	</head>
+	<body style="font-size: large">
+		<div>
+			<a href="moblayout1.pdf">Mobile Layout1</a><br>
+		</div>
+		<div>
+			<a href="moblayout2.pdf">Mobile Layout2 </a><br>
+		</div>
+
+		<div>
+			<img src="mobile layout1 .png" alt="mobilelayout" height="500">
+            <br>
+		</div>
+
+		<div>
+			<img src="mobile layout2.png" alt="mobilelayout" height="500">
+           <br>
+		</div>
+
+		<div>
+			<a href="index.php">HomePage</a>
+		</div>
+	</body>
+</html>
+
